27b48212ec446b54b8b31fe974392198d883edbd
Sam Decrock <sam.decrock@gmail.com>
2013-07-20 15:19:37 +0100

    config stuff in config.js gestoken

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var http = require('http')
 var path = require('path');
 
+var config = require('./config');
 var lights = require('./lights');
 
 /*
@@ -37,43 +38,18 @@ app.get('/', function(req, res){
 });
 */
 
-var player2lightid = {
-	'matt' : 1,
-	'robby': 2,
-	'sam'  : 3
-};
-
-//flag.i is een beetje hacky :p
-var flags = {
-	A  : {i: 0, code: 'A', name:'Agility', hue: 0},
-	CL : {i: 1, code: 'CL', name:'Cloaking', hue: 0},
-	G  : {i: 2, code: 'G', name:'Genocide', hue: 0},
-	GM : {i: 3, code: 'GM', name:'Guided Missile', hue: 0},
-	L  : {i: 4, code: 'L', name:'Laser', hue: 0},
-	OO : {i: 5, code: 'OO', name:'Oscillation Overthruster', hue: 0},
-	F  : {i: 6, code: 'F', name:'Rapid Fire', hue: 0},
-	R  : {i: 7, code: 'R', name:'Ricochet', hue: 0},
-	SH : {i: 8, code: 'SH', name:'Shield', hue: 0},
-	SW : {i: 9, code: 'SW', name:'Shock Wave', hue: 0},
-	SR : {i: 10, code: 'SR', name:'Steam Roller', hue: 0},
-	SB : {i: 11, code: 'SB', name:'Super Bullet', hue: 0},
-	T  : {i: 12, code: 'T', name:'Tiny', hue: 0},
-	V  : {i: 13, code: 'V', name:'High Speed', hue: 0},
-	WG : {i: 14, code: 'WG', name:'Wings', hue: 0}
-}
-
 
 
 function flagGrabbed(player, flagid){
-	var lightid = player2lightid[player];
-	var flag = flags[flagid];
+	var lightid = config.player2lightid[player];
+	var flag = config.flags[flagid];
 
 	if(lightid == null) return console.log('unknown player: ' + player);
 	if(flag == null) return console.log('unknown flag: ' + flagid);
 
 	if(flag.hue == 0){
 		// berekeen aan de hand van index
-		var nrOfFlags = Object.keys(flags).length;
+		var nrOfFlags = Object.keys(config.flags).length;
 		flag.hue = Math.round(65535/nrOfFlags * flag.i);
 	}
 
---
+++ app/config.js
@@ -0,0 +1,24 @@
+exports.player2lightid = {
+	'matt' : 1,
+	'robby': 2,
+	'sam'  : 3
+};
+
+//flag.i is een beetje hacky :p
+exports.flags = {
+	A  : {i: 0, code: 'A', name:'Agility', hue: 0},
+	CL : {i: 1, code: 'CL', name:'Cloaking', hue: 0},
+	G  : {i: 2, code: 'G', name:'Genocide', hue: 0},
+	GM : {i: 3, code: 'GM', name:'Guided Missile', hue: 0},
+	L  : {i: 4, code: 'L', name:'Laser', hue: 0},
+	OO : {i: 5, code: 'OO', name:'Oscillation Overthruster', hue: 0},
+	F  : {i: 6, code: 'F', name:'Rapid Fire', hue: 0},
+	R  : {i: 7, code: 'R', name:'Ricochet', hue: 0},
+	SH : {i: 8, code: 'SH', name:'Shield', hue: 0},
+	SW : {i: 9, code: 'SW', name:'Shock Wave', hue: 0},
+	SR : {i: 10, code: 'SR', name:'Steam Roller', hue: 0},
+	SB : {i: 11, code: 'SB', name:'Super Bullet', hue: 0},
+	T  : {i: 12, code: 'T', name:'Tiny', hue: 0},
+	V  : {i: 13, code: 'V', name:'High Speed', hue: 0},
+	WG : {i: 14, code: 'WG', name:'Wings', hue: 0}
+}
