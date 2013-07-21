7d82b6f6fbb138dbf17882ce905c41b5b6d2010c
Matthias De Geyter <matthias.degeyter@ibbt.be>
2013-07-21 05:25:06 +0100

    free some memory from time to time

--- bzplugin/eventlogger.cpp
+++ bzplugin/eventlogger.cpp
@@ -6,15 +6,20 @@
 // event handler callback
 #include "cURLManager.h"
 
+
+
+
 class EventPoster : cURLManager {
 public:
   EventPoster();
   void addKeyValuePair(const char *key, const char *value);
   void postStuff();
+  virtual void finalization(char *data, unsigned int length, bool good);
+  static int requestCounter;
 };
 
 EventPoster::EventPoster() : cURLManager(){
-  setURL("http://10.0.1.6:3000/bzflag");
+  setURL("http://10.0.1.5:3000/bzflag");
 }
 
 void EventPoster::addKeyValuePair(const char *key, const char *value)
@@ -24,7 +29,15 @@ void EventPoster::addKeyValuePair(const char *key, const char *value)
 
 void EventPoster::postStuff(){
   setHTTPPostMode();
+  requestCounter++;
   addHandle();
+}
+
+int EventPoster::requestCounter = 0;
+
+void EventPoster::finalization(char *data, unsigned int length, bool good)
+{
+  requestCounter--;
 }
 
 class EventLogger : public bz_Plugin
@@ -36,6 +49,8 @@ public:
   virtual void Cleanup ( void );
   virtual void Event ( bz_EventData *eventData );
 };
+
+static std::vector<EventPoster*> epVector;
 
 BZ_PLUGIN(EventLogger)
 
@@ -51,6 +66,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_eFlagGrabbedEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_FlagGrabbedEventData_V1* fge = (bz_FlagGrabbedEventData_V1*)eventData;
     int p = fge->playerID;
     bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
@@ -68,6 +84,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_eFlagDroppedEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_FlagDroppedEventData_V1* fde = (bz_FlagDroppedEventData_V1*)eventData;
     int p = fde->playerID;
     bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
@@ -81,7 +98,20 @@ void EventLogger::Event(bz_EventData *eventData)
   }
 
   case bz_eShotFiredEvent: {
+    char buf[8];
+    sprintf(buf, "rc %d", EventPoster::requestCounter);
+    bz_debugMessage(1, buf);
+    if(EventPoster::requestCounter==0){
+      int  texNo = epVector.size();
+      char vs[8];
+      sprintf(vs, "vs %d", texNo);
+      bz_debugMessage(1, vs);
+      for (int i = 0; i < texNo; i++)
+        delete epVector[i];
+        epVector.clear();
+    }
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_ShotFiredEventData_V1* sfed = (bz_ShotFiredEventData_V1*)eventData;
     int p = sfed->playerID;
     bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
@@ -91,13 +121,16 @@ void EventLogger::Event(bz_EventData *eventData)
     evPoster->addKeyValuePair("player", playerRecord->callsign.c_str());
     evPoster->addKeyValuePair("flag", sfed->type.c_str());
     evPoster->postStuff();
-
-    // bz_setPlayerWins(p, player->wins+10);
+    // if(playerRecord->callsign.c_str()[0] == 'm'){
+    //   bz_setPlayerWins(p, playerRecord->wins+50);
+    //    bz_debugMessage(1, "matthias shoots");
+    // }
     break;
   }
 
   case bz_ePlayerDieEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_PlayerDieEventData_V1* deed = (bz_PlayerDieEventData_V1*)eventData;
     bz_ApiString flag = deed->flagKilledWith;
     int p = deed->playerID;
@@ -117,6 +150,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_eGameEndEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     evPoster->addKeyValuePair("action", "gameend");
     evPoster->postStuff();
 
@@ -125,12 +159,14 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_eGameStartEvent: {
     EventPoster*  evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     evPoster->addKeyValuePair("action", "gamestart");
     evPoster->postStuff();
   }
 
   case bz_eGetPlayerInfoEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_GetPlayerInfoEventData_V1* pie = (bz_GetPlayerInfoEventData_V1*)eventData;
     char team[7];
     evPoster->addKeyValuePair("action", "playerinfo");
@@ -148,6 +184,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_ePlayerJoinEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_PlayerJoinPartEventData_V1* pje = (bz_PlayerJoinPartEventData_V1*)eventData;
     bz_BasePlayerRecord *playerRecord = pje->record;
     evPoster->addKeyValuePair("action", "join");
@@ -159,6 +196,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_ePlayerPartEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_PlayerJoinPartEventData_V1* pje = (bz_PlayerJoinPartEventData_V1*)eventData;
     bz_BasePlayerRecord *playerRecord = pje->record;
     evPoster->addKeyValuePair("action", "part");
@@ -170,6 +208,7 @@ void EventLogger::Event(bz_EventData *eventData)
 
   case bz_ePlayerSpawnEvent: {
     EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
     bz_PlayerSpawnEventData_V1* pse = ( bz_PlayerSpawnEventData_V1*)eventData;
     int p = pse->playerID;
     bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
@@ -189,12 +228,63 @@ void EventLogger::Event(bz_EventData *eventData)
 
   }
 
+  case bz_eServerMsgEvent: {
+    EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
+    bz_ServerMsgEventData_V1* se =(bz_ServerMsgEventData_V1*) eventData;
+    int p = se->to;
+    bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
+    if (!playerRecord) break;
+    evPoster->addKeyValuePair("action", "servermessage");
+    evPoster->addKeyValuePair("to", playerRecord->callsign.c_str());
+    evPoster->addKeyValuePair("message", se->message.c_str());
+    evPoster->postStuff();
+
+    break;
+  }
+
+  case bz_eGetAutoTeamEvent: {
+    EventPoster* evPoster = new EventPoster();
+    epVector.push_back(evPoster);
+    bz_GetAutoTeamEventData_V1* at = (bz_GetAutoTeamEventData_V1*) eventData;
+    evPoster->addKeyValuePair("action", "autoteam");
+    evPoster->addKeyValuePair("player", at->callsign.c_str());
+    char team[7];
+    if(at->team == eRabbitTeam)
+      strcpy(team, "rabbit");
+    if(at->team == eHunterTeam)
+      strcpy(team, "hunter");
+    evPoster->addKeyValuePair("team", team);
+    evPoster->postStuff();
+
+    break;
+
+  }
+
+  case bz_ePlayerScoreChanged: {
+    bz_PlayerScoreChangeEventData_V1* ps = (bz_PlayerScoreChangeEventData_V1*) eventData;
+    int p = ps->playerID;
+    bz_BasePlayerRecord *playerRecord = bz_getPlayerByIndex(p);
+    if (!playerRecord) break;
+    if(playerRecord->team == eRabbitTeam){
+        EventPoster* evPoster2 = new EventPoster();
+        epVector.push_back(evPoster2);
+        evPoster2->addKeyValuePair("action", "israbbit");
+        evPoster2->addKeyValuePair("player", playerRecord->callsign.c_str());
+        evPoster2->postStuff();
+    }
+    break;
+
+
+  }
+
   }
 }
 
 void EventLogger::Init ( const char* /*commandLine*/ )
 {
   EventPoster* evPoster = new EventPoster();
+  epVector.push_back(evPoster);
   evPoster->addKeyValuePair("action", "start");
   evPoster->postStuff();
   bz_debugMessage(1, "eventlogger initialized");
@@ -209,13 +299,16 @@ void EventLogger::Init ( const char* /*commandLine*/ )
   Register(bz_ePlayerJoinEvent);
   Register(bz_ePlayerPartEvent);
   Register(bz_ePlayerSpawnEvent);
+  // Register(bz_eServerMsgEvent);
+  // Register(bz_eGetAutoTeamEvent);
+  // Register(bz_ePlayerScoreChanged);
 }
 
 void EventLogger::Cleanup ( void )
 {
-  EventPoster* evPoster = new EventPoster();
-  evPoster->addKeyValuePair("action", "stop");
-  evPoster->postStuff();
+
+
+
   // unregister our events
   Flush();
   bz_debugMessage(1, "eventlogger plugin unloaded");
